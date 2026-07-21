46f226200ea33ea038f07d917b18d65a6c2371e3
Willzthegreat <obiadichinwuba@yahoo.com>
2026-07-21 08:10:32 +0100

    3rd Update on the 21th July 2026

--- frankobiadi/components/logo.tsx
+++ frankobiadi/components/logo.tsx
@@ -1,14 +1,13 @@
-import Link from 'next/link'
-import React from 'react'
-
-const Logo = () => {
-  return (
-    <>
-      <Link href="/" className="text-lg font-bold">
-        FRANK OBIADI
-      </Link>
-    </>
-  )
+interface LogoProps {
+  style?: string;
 }
 
-export default Logo
+const Logo = ({ style = "" }: LogoProps) => {
+  return (
+    <h1 className={style}>
+      FRANK OBIADI
+    </h1>
+  );
+};
+
+export default Logo;
