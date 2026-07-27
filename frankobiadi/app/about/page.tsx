2abdbd9540f4c98eeab64722602bcad05e7e2bee
Willzthegreat <obiadichinwuba@yahoo.com>
2026-07-27 01:40:41 +0100

    Update on the 26th July 2026

--- frankobiadi/app/about/page.tsx
+++ frankobiadi/app/about/page.tsx
@@ -7,24 +7,7 @@ import BuiltImage from "@/public/buildTogether.png";
 const About = () => {
   return (
     <div>
-      <AfricanGreatestOpportunity
-        text="Building the Foundations of African Enterprise"
-        headingClassName="mt-20 md:mt-20 text-5xl font-bold lg:text-6xl"
-        image={BuiltImage}
-        imageClassName="hidden md:block -mt-56 sm:-mt-56 w-32 md:w-48 lg:w-64"
-        paragraphClassName="max-w-xl text-sm leading-9 mt-20 lg:mt-56 text-gray-100/60"
-        ptext={
-          <>
-            I build more than companies.
-            <br />
-            <br />
-            I build ventures, partnerships and business infrastructure that
-            create the conditions for resilient businesses, stronger markets
-            and long-term economic transformation across Africa.
-          </>
-        }
-      />
-      <AboutGrid   />
+     
       <Gallery />
       <BuildTogetherCom showImage={false} />
     </div>
--- frankobiadi/app/works/page.tsx
+++ frankobiadi/app/works/page.tsx
@@ -1,34 +1,34 @@
-import AfricanGreatestOpportunity from '@/components/africanGreatestOportunity'
-import BuildTogetherCom from '@/components/buildTogetherCom'
-import Gallery from '@/components/gallery'
-import React from 'react'
-import BuiltImage from "@/public/Company.png";
-import WorkLastingValue from '@/components/workLastingValue';
-import WorkGrid from '@/components/workGrid';
+import AboutGrid from "@/components/aboutGrid";
+import AfricanGreatestOpportunity from "@/components/africanGreatestOportunity";
+import BuildTogetherCom from "@/components/buildTogetherCom";
+import Gallery from "@/components/gallery";
+import BuiltImage from "@/public/buildTogether.png";
 
-const Page = () => {
+const Work = () => {
   return (
-    <>
-      <div>
-        <AfricanGreatestOpportunity
-          text="Building Ventures That Strengthen African Enterprise"
-          headingClassName="mt-20 md:mt-20 text-5xl font-bold lg:text-6xl"
-          image={BuiltImage}
-          imageClassName="hidden md:block -mt-56 sm:-mt-56 w-42 md:w-48 lg:w-64"
-          paragraphClassName="max-w-xl text-sm leading-9 mt-20 lg:mt-56 text-gray-100/60"
-          ptext={
-            <>
-            Every venture I build begins with one question:
-            </>
-          }
-        />
-        <WorkLastingValue  />
-        <WorkGrid />
-        <Gallery />
-        <BuildTogetherCom showImage={false} />
-      </div>
-    </>
-  )
-}
+    <div>
+      <AfricanGreatestOpportunity
+        text="Building the Foundations of African Enterprise"
+        headingClassName="mt-20 md:mt-20 text-5xl font-bold lg:text-6xl"
+        image={BuiltImage}
+        imageClassName="hidden md:block -mt-56 sm:-mt-56 w-32 md:w-48 lg:w-64"
+        paragraphClassName="max-w-xl text-sm leading-9 mt-20 lg:mt-56 text-gray-100/60"
+        ptext={
+          <>
+            I build more than companies.
+            <br />
+            <br />
+            I build ventures, partnerships and business infrastructure that
+            create the conditions for resilient businesses, stronger markets
+            and long-term economic transformation across Africa.
+          </>
+        }
+      />
+      <AboutGrid   />
+      <Gallery />
+      <BuildTogetherCom showImage={false} />
+    </div>
+  );
+};
 
-export default Page
+export default Work;
